0151c5b86b674ed81b2181b8277af0e86dde80e8
Igor Gramkow <gramkowigor@gmail.com>
2025-07-23 09:51:33 -0300

    Docker Compose With API and UI

--- ui/src/screens/note/index.jsx
+++ ui/src/screens/note/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getNoteByLink } from "../../api/notes";
+import { getNoteByLink, updateNoteContent } from "../../api/notes";
 import { NoteWritter } from "../../components/writters/NoteWritter";
 import HtmlWritter from "../../components/writters/HtmlWritter";
 import MdWritter from "../../components/writters/MdWritter";
@@ -10,10 +10,35 @@ export function Note() {
   const { link } = useParams();
   const [noteInfos, setNoteInfos] = useState(null);
   const [content, setContent] = useState("");
+  const [lastContent, setLastContent] = useState(null);
+  const [saveContent, setSaveContent] = useState(false);
 
   useEffect(() => {
     handleNote()
   }, [link])
+
+  useEffect(() => {
+    const interval = setInterval(() => { setSaveContent(true) }, 3000)
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    if (saveContent) {
+      handleUpdateContent()
+      setSaveContent(false)
+    }
+  }, [saveContent])
+
+  async function handleUpdateContent() {
+    if (lastContent == content) return
+    const payload = {
+      id: noteInfos.id,
+      content: content
+    }
+    await updateNoteContent(payload)
+    setLastContent(content)
+    setSaveContent(false)
+  }
 
   async function handleNote() {
     if (!link) return
